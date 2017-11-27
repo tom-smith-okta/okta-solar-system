90ad40fa0d1a24b6803e07e7935a326688d87cb4
Tom Smith <tom.smith@okta.com>
2017-11-27 09:27:33 -0800

    added CORS function

--- app.js
+++ app.js
@@ -12,6 +12,25 @@ const express = require('express');
 // SET UP WEB SERVER
 const app = express();
 
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3090');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
 require(__base + 'routes.js')(app);
 
 var port = process.env.PORT || 5000;
