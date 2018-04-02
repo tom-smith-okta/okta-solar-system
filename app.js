fadd25883c3557cf7d027f05fbb86e8f033d14be
Tom Smith <tom.smith@okta.com>
2018-04-02 09:30:59 -0400

    more cleanup

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
-// Okta Access Management
+// Okta Solar System API
 
 ////////////////////////////////////////////////////
-// global.__base = __dirname + '/';
-
-// var config = require(__base + ".env.js");
 
 const express = require('express');
 
@@ -12,28 +9,28 @@ const express = require('express');
 // SET UP WEB SERVER
 const app = express();
 
-app.use(function (req, res, next) {
+// app.use(function (req, res, next) {
 
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*');
+//     // Website you wish to allow to connect
+//     res.setHeader('Access-Control-Allow-Origin', '*');
 
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+//     // Request methods you wish to allow
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,Authorization');
+//     // Request headers you wish to allow
+//     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,Authorization');
 
-    // // Set to true if you need the website to include cookies in the requests sent
-    // // to the API (e.g. in case you use sessions)
-    // res.setHeader('Access-Control-Allow-Credentials', true);
+//     // // Set to true if you need the website to include cookies in the requests sent
+//     // // to the API (e.g. in case you use sessions)
+//     // res.setHeader('Access-Control-Allow-Credentials', true);
 
-    // Pass to next layer of middleware
-    next();
-});
+//     // Pass to next layer of middleware
+//     next();
+// });
 
 require('./routes.js')(app);
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 3000;
 
 app.listen(port, function () {
 	console.log('App listening on port ' + port);
