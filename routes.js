6a378ca26c06dd7260608f69922a88c216a50d88
Tom Smith <tom.smith@okta.com>
2019-04-16 14:26:57 -0400

    add asteroids endpoint

--- routes.js
+++ routes.js
@@ -7,16 +7,23 @@ module.exports = function (app) {
 
 	})
 
+	app.get('/asteroids', function(req, res, next) {
+
+		var asteroids = ["ceres", "hygiea", "pallas", "vesta"]
+
+		res.json(moons)
+	}
+
 	app.get('/planets', function(req, res, next) {
 
-		var planets = [ "mercury", "venus", "earth", "mars", "jupiter", "saturn", "uranus", "neptune"]
+		var planets = ["mercury", "venus", "earth", "mars", "jupiter", "saturn", "uranus", "neptune"]
 
 		res.json(planets)
 	})
 
 	app.get('/moons', function(req, res, next) {
 
-		var moons = [ "adrastea", "amalthea", "callisto", "europa", "ganymede", "io", "metis", "thebe"]
+		var moons = ["adrastea", "amalthea", "callisto", "europa", "ganymede", "io", "metis", "thebe"]
 
 		res.json(moons)
 	})
