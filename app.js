2091437099e431356ad677de6531fd981249f8c3
Tom Smith <tom.smith@okta.com>
2020-11-06 08:41:14 -0500

    put express.json in right place

--- app.js
+++ app.js
@@ -9,11 +9,11 @@ const express = require('express');
 // SET UP WEB SERVER
 const app = express();
 
+app.use(express.json());
+
 require('./routes.js')(app);
 
 var port = process.env.PORT || 3228;
-
-app.use(express.json());
 
 app.listen(port, function () {
 	console.log('App listening on port ' + port);
